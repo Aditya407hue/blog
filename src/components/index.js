7ff541cc4445089e67aff5e693094a419096107c
Aditya Singh <aditya2002sjv@gmail.com>
2023-10-07 20:56:25 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Select from './Select'
+import Header from "./Header/Header.jsx";
+import Footer from "./Footer/Footer.jsx";
+import Container from './container/Container.jsx'
+import Logo from "./Logo.jsx";
+import LogoutBtn from "./Header/LogoutBtn.jsx";
+import RTE from "./RTE.jsx"
+import Signup from './Signup.jsx'
+import Login from './Login.jsx'
+import Button from './Button.jsx'
+import PostForm from './post-form/PostForm.jsx'
+import PostCard from './PostCard.jsx'
+import AuthLayout from './AuthLayout.jsx'
+import Input from './Input.jsx'
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    Button,
+    PostForm,
+    PostCard,
+    AuthLayout,
+    Input,
+    Select,
+}
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Signup as SingupComponent } from '../components'
+
+function Signup() {
+  return (
+    <div className='py-8'>
+        <SingupComponent/>
+        </div>
+  )
+}
+
+export default Signup
